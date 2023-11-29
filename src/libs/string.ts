8a87f98694d45327e676003aed66c6703645a313
Artur Gudiev <arturgudiev93@gmail.com>
2023-11-29 15:13:29 +0300

    Change method name

--- src/libs/string.ts
+++ src/libs/string.ts
@@ -1,6 +1,6 @@
 import { getUserInput } from "./io.lib";
 
-export async function appendValue(originalValue: string, options: {
+export async function appendStringValue(originalValue: string, options: {
     userValue?: string;
     newLine: boolean
 } = {newLine: true}): Promise<string> {
