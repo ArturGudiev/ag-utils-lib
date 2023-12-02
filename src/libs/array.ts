26137ebf965cf66a0e1cc1e05a2bf5a52f5e88fa
Artur Gudiev <arturgudiev93@gmail.com>
2023-12-02 11:53:55 +0300

    Add addElementToObjectArrayOrCreateOne

--- src/libs/array.ts
+++ src/libs/array.ts
@@ -13,3 +13,11 @@ export function positionIsOutOfArray(arr: any[], position: number): boolean {
 export function equalArraysContent(arr1: any[], arr2: any[]) {
     return _.isEqual(arr1.sort(), arr2.sort());
 }
+
+export function addElementToObjectArrayOrCreateOne(obj: any, key: string, el: any) {
+    if ( obj[key] === undefined ) {
+        obj[key] = [el];
+    } else {
+        obj[key].push(el)
+    }
+}
