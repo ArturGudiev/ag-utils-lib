b92f06d74b9d9e4df45158a2fffa7035e5e9cf4a
Artur Gudiev <arturgudiev93@gmail.com>
2023-12-02 11:54:07 +0300

    And tsc

--- lib/libs/array.d.ts
+++ lib/libs/array.d.ts
@@ -1,3 +1,4 @@
 export declare function removeFirstArgument(args: any[]): any[];
 export declare function positionIsOutOfArray(arr: any[], position: number): boolean;
 export declare function equalArraysContent(arr1: any[], arr2: any[]): boolean;
+export declare function addElementToObjectArrayOrCreateOne(obj: any, key: string, el: any): void;
